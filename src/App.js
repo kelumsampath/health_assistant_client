51fe2fac09f5b4ffc561e01c1e26ad947e2e7fe0
sooraj <sooraj1998ayiroor@gmail.com>
2020-10-18 11:13:51 +0530

    home and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from "react";
-import logo from "./images/logo.svg";
-import "./App.css";
+import React, {  Component }  from 'react';
+import './App.css';
+import Main from './components/HeaderComponent';
+import { BrowserRouter } from 'react-router-dom';
+class App extends Component {
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Health Assistant App</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+  render() {
+    return (
+		<BrowserRouter>
+		<div className="App">
+		<Main />
 		</div>
-	);
+		</BrowserRouter>
+    );
+  }
 }
-
 export default App;
