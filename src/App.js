04130913fa4e9e742a25f3cfd7929765517ec7d2
kelumsampath <edirisingheemks@gmail.com>
2020-10-29 09:34:11 +0530

    codebase refacrored

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-import logo from "./images/logo.svg";
-import "./App.css";
+import HomePage from './container/HomePage'
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Health Assistant App</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+export default class App extends React.Component {
+	render(){
+		return (
+			<HomePage></HomePage>
+		);
+	}
 }
-
-export default App;
