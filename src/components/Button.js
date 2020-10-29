22db35295088d85cae16b9596b5aad83ca2e0acb
kelumsampath <edirisingheemks@gmail.com>
2020-10-29 09:43:41 +0530

    minor changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,1 @@
-// If you need similar small helps, just hmu. I'll try to help you so that you can focus on major development.
-// This is my first time doing this type of contributing (first time ever PR), so yep will gladly help. I also know MERN and MEAN (can help those too if you too tired to implement lol)(Check my GitHub Side-Projects). Thank you!
-
-// Also removed some emptyspaces to make the code look more clean (CTRL S with Prettier)
+//use class functional component
